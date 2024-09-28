370accc49904960dc75b9d9fc9eff4c518770e25
CSTL <raghu@chandusoft.net>
2024-09-28 15:53:01 +0530

    GrabOnJenkins

--- src/test/java/tests/StoresInfoTest.java
+++ src/test/java/tests/StoresInfoTest.java
@@ -2,12 +2,23 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
 
 public class StoresInfoTest {
 	
+	WebDriver driver;
+	
+	@AfterMethod
+	public void tearDown() {
+		
+		driver.quit();
+	}
+	
+	@Test
 	public void StoresInfo() {
 	
-	WebDriver driver = new ChromeDriver();
+	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.grabon.in/stores/");
 }
